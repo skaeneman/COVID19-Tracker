893192acf7ec7ff985fddf207ffa2e21fbd29563
scott <19240759+skaeneman@users.noreply.github.com>
2020-05-19 13:55:37 -0400

    update styling of bootstrap cards for showing state data

--- react/ReactCovid19Tracker/src/components/StatePolicyData.js
+++ react/ReactCovid19Tracker/src/components/StatePolicyData.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 
 export default function StatePolicyData(props) {
   
@@ -19,10 +19,18 @@ export default function StatePolicyData(props) {
           <Card.Header>State Data</Card.Header>
           <Card.Body>
             <Card.Title>State Data</Card.Title>
-            <Card.Text>
+            <Card.Text as="span">
+            <div>
             {/* id: { state.id } */}
-            id: { props.stateData.id }
-            state: { props.stateData.stateName }
+              <Row>
+                <Col xs="2" className="text-left">id:</Col>
+                <Col className="text-left">{ props.stateData.id }</Col>
+              </Row> 
+              <Row>
+                <Col xs="2" className="text-left">state:</Col>
+                <Col className="text-left">{ props.stateData.stateName }</Col>
+              </Row>
+            </div>
             </Card.Text>
           </Card.Body>
           <Card.Footer className="text-muted"></Card.Footer>
