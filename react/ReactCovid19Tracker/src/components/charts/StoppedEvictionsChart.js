93f515badbe56e5363049642694e182a91a7d017
scott <19240759+skaeneman@users.noreply.github.com>
2020-05-26 21:57:41 -0400

    created a component for the first chart

---
+++ react/ReactCovid19Tracker/src/components/charts/StoppedEvictionsChart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function StoppedEvictionsChart() {
+  return (
+    <div>
+      <p className="text-white">test...</p>
+    </div>
+  )
+}
--- react/ReactCovid19Tracker/src/components/pages/ChartPage.js
+++ react/ReactCovid19Tracker/src/components/pages/ChartPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import StoppedEvictionsChart from '../charts/StoppedEvictionsChart';
 
 export default class ChartPage extends Component {
   render() {
     return (
       <div>
-        
+        <StoppedEvictionsChart />
       </div>
     )
   }
