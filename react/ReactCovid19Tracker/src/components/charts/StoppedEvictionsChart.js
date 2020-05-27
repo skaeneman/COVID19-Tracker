f9565717b553c54ef5ecb9af8abf04f9b9c8eed8
scott <19240759+skaeneman@users.noreply.github.com>
2020-05-27 00:00:23 -0400

    created a test chart using Chart.js with some placeholder data.  Have to create an API call later

--- react/ReactCovid19Tracker/src/components/charts/StoppedEvictionsChart.js
+++ react/ReactCovid19Tracker/src/components/charts/StoppedEvictionsChart.js
@@ -1,9 +1,103 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Doughnut, Bar, Line, Pie, Scatter } from 'react-chartjs-2';
 export default function StoppedEvictionsChart() {
+
+  // temporarily set the page to white background
+  // document.body.style.backgroundColor = "#FFFFFF";
+
+
+  const [chartData, setChartData] = useState({}); // empty object
+
+  const chart = () => {
+    const labels = [
+      "Boston",
+      "Worcester",
+      "Cambridge"
+    ];
+
+    setChartData({
+      labels,
+      datasets: [
+        {
+          label: "Ratings",
+          data: [10, 5, 12],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],  
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1                  
+        },
+      ],
+     
+    })
+  };
+
+  useEffect(() => {
+    chart(); // call function
+  }, []);
+
+
   return (
     <div>
       <p className="text-white">test...</p>
+      { console.log(chartData) }
+
+      <Bar
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { 
+              display: true,
+              fontColor: '#FFFFFF',
+              text: 'Test chart...'
+            },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true,
+                    fontColor: "white",
+                  },
+                  gridLines: {
+                    display: true,
+                    color: "#495057"
+                  },
+                },
+              ],
+              xAxes: [
+                {
+                  ticks: {
+                    fontColor: "#FFFFFF",
+                  },                  
+                  gridLines: {
+                    display: true,
+                    color: "#495057"
+                  },
+                },
+              ],
+            }, 
+            legend: {
+              labels: {
+                   fontColor: '#FFFFFF'
+                  }
+               },
+
+          }}
+        />
     </div>
   )
 }
--- react/ReactCovid19Tracker/src/components/pages/ChartPage.js
+++ react/ReactCovid19Tracker/src/components/pages/ChartPage.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import StoppedEvictionsChart from '../charts/StoppedEvictionsChart';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default class ChartPage extends Component {
   render() {
     return (
       <div>
-        <StoppedEvictionsChart />
+        
+        {/* <Doughnut data={...} /> */}
+        <Container>
+          <br />
+          <Row>
+            <Col as={Col} sm="12" >
+              <StoppedEvictionsChart />
+            </Col>                     
+          </Row>
+          <br />
+        </Container>
+
       </div>
     )
   }
